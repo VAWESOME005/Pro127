1e0bac743ba9d4d7d8acdc246e69bf7bd059b233
VAWESOME005 <40074540+VAWESOME005@users.noreply.github.com>
2022-06-13 10:49:59 -0700

    Update pro127.py

--- pro127.py
+++ pro127.py
@@ -3,10 +3,10 @@ import requests
 import pandas as pd
 
 
-START_URL = (
-    "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
-)
-wiki = requests.get(START_URL)
+start_url = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+
+
+wiki = requests.get(start_url)
 soup = BS(wiki.text, "html.parser")
 temp_list = []
 for tr in soup.find("table").find_all("tr"):
@@ -20,13 +20,21 @@ mass = []
 radius = []
 
 for i in range(1, len(temp_list)):
+    
     name.append(temp_list[i][1])
+    
     distance.append(temp_list[i][3])
+    
     mass.append(temp_list[i][5])
+    
     radius.append(temp_list[i][6])
 
+    
 df = pd.DataFrame(
+    
     list(zip(name, distance, mass, radius)),
+    
     columns=["Star_name", "Distance", "Mass", "Radius"],
 )
+
 df.to_csv("pro127Scraper.csv")
